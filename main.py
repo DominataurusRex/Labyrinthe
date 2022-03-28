b0fa7671db9acd3b1d789068071f62832af09eea
DominataurusRex <95956715+DominataurusRex@users.noreply.github.com>
2022-03-28 10:08:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import pygame
 from Modules.affichage import main_menu
+from Modules.fonction import init_texture
 
 pygame.init()
 window = pygame.display.set_mode((960, 510), pygame.RESIZABLE)
 color = (0, 'BLUE')
 
-def temp():
-    liste = []
-    for ligne in range(10):
-        temp = []
-        for colonne in range(10):
-            temp.append('0')
-        liste.append(temp)
-    for l in liste:
-        print(l)
-
-main_menu(window, color)
+texture = init_texture(window)
+main_menu(window, texture, color)
